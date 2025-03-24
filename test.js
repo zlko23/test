6dc697a23c6dbba1d0769e6f96d7f453a867be03
Zlko <107702558+zlko23@users.noreply.github.com>
2025-03-24 10:43:10 -0300

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+let num1 = 100;
+let num2 = 500;
+
+let suma = num1 + num2;
+let resta = suma - num1 - num1;
+let mult = num1 * 7;
+let div = num2 / num1;
+
+console.log("Suma: ", suma);
+console.log("Resta: ", suma);
+console.log("Multiplicacion: ", suma);
+console.log("Division: ", suma);
